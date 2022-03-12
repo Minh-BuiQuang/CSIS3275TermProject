a2ef2c93096548f0217e056ebc1f345c9c2fb09d
Minh Bui <bqminh1806@gmail.com>
2022-03-11 21:57:35 -0800

    Remove ProductId from Supplier/Customer dto classes

--- Application/WebAPI/StockInventoryWebApi/StockInventoryWebApi.BusinessModel/DataTransferModel/CustomerDto.cs
+++ Application/WebAPI/StockInventoryWebApi/StockInventoryWebApi.BusinessModel/DataTransferModel/CustomerDto.cs
@@ -11,6 +11,5 @@ namespace StockInventoryWebApi.BusinessModel.DataTransferModel
         public string Location { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public int ProductId { get; set; }
     }
 }
