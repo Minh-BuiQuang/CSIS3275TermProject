a31a7c725342065f66149d401e9ea3e9eaf5536e
Minh Bui <bqminh1806@gmail.com>
2022-03-16 21:48:30 -0700

    Add StockOut logic, display current quantity of the product, and remove some logging.

--- clothing-stock-management/src/components/StockIn.jsx
+++ clothing-stock-management/src/components/StockIn.jsx
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useState, useRef} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import InventoryContext from '../Context/InventoryContext';
 
 function StockIn() {
@@ -8,11 +8,12 @@ function StockIn() {
     const [productId, setProductId] = useState("");
     const [productName, setProductName] = useState("");
     const [category, setCategory] = useState([]);
-    const [supplier, setSuppliers] = useState([]);
-    const [selectedSupplier, setSelectedSupplier] = useState([]);
+    const [supplier, setSupplier] = useState([]);
+    const [selectedSupplier, setSelectedSupplier] = useState("");
     const [quantity, setQuantity] = useState(0);
     const [description, setDescription] = useState("");
     const [comments, setComments] = useState("");
+    const [currentQuantity, setCurrentQuantity] = useState("");
 
     const clearFields = () => {
         
@@ -24,12 +25,13 @@ function StockIn() {
         setDescription("");
         setSelectedSupplier("");
         setComments("");
+        setCurrentQuantity("");
     }
     useEffect(()=>{
         const fetchSuppliers = async () => {
             const response = await fetch('https://localhost:44348/api/Supplier');
             const data = await response.json();
-            setSuppliers(data.data);
+            setSupplier(data.data);
         };
         fetchSuppliers();
     }, [])
@@ -41,13 +43,17 @@ function StockIn() {
             return;
         }
         const response = await fetchProductDetails(e.target.value);
-        if(response.success)
-        {
+        if(response.success) {
             const productDetails = response.data;
             setProductName(productDetails.productName);
             setCategory(productDetails.category);
             setQuantity(0);
+            setCurrentQuantity(productDetails.quantity);
             setDescription(productDetails.description);
+        }
+        else {
+            alert('No product with id ' + e.target.value + ' was found!');
+            clearFields();
         }
     }
 
@@ -66,9 +72,8 @@ function StockIn() {
     // add new stock to record
     const handleAddToStocks = (e) => {
         e.preventDefault();
-        if(!productId ||
-           quantity<=0) {
-               window.alert("Please give required fields");
+        if(productId.trim()==="" || quantity<=0 || isNaN(quantity)) {
+            window.alert("Please give required fields");
             return;
         }
         const newStock = {
@@ -88,8 +93,8 @@ function StockIn() {
         <div className="row">
             <form className="col-4" onSubmit={handleAddToStocks}>
                 <input value={productId} onChange={handleProductIdChange} type="text" className="form-control form-control-lg mb-3" placeholder="Product ID"/>
-                <input value={productName} type="text" readonly="readonly" className="form-control form-control-lg mb-3" placeholder="Product Name"/>
-                <input value={category} type="text" readonly="readonly" className="form-control form-control-lg mb-3" placeholder="Category Name"/>
+                <input value={productName} type="text" readOnly="readonly" className="form-control form-control-lg mb-3" placeholder="Product Name"/>
+                <input value={category} type="text" readOnly="readonly" className="form-control form-control-lg mb-3" placeholder="Category Name"/>
                 <select value={selectedSupplier} className="form-control form-control-lg custom-select custom-select-lg mb-3" onChange={handleSupplierChange}>
                     <option value="">Select Supplier</option>
                     {supplier.map(i=>(<option key={i.supplierId} value={i.supplierId}>{i.supplierName}</option>))}
@@ -98,7 +103,8 @@ function StockIn() {
                 <input type="submit" className="btn btn-lg btn-success w-100 mb-3" value="Add To Stocks" />
             </form> 
             <div className="col-8 ">
-                <textarea className={"w-100 h-75 description-box "+(description?"text-dark":"text-muted")} value={description} readonly="readonly" placeholder="Add Product ID to view product details" />
+                <p type="text">Current quantity: {currentQuantity}</p>
+                <textarea className={"w-100 h-75 description-box "+(description?"text-dark":"text-muted")} value={description} readOnly="readonly" placeholder="Add Product ID to view product details" />
                 <textarea className={"w-100 h-25 "+(comments?"text-dark":"text-muted")} value={comments} onChange={handleCommentsChange} placeholder="Leave a comment for this transaction." />
             </div>
         </div>   
