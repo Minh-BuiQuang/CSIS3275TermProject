b90267217de57c0c96d1e1994e495cfea58c7bf6
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2022-03-17 00:55:01 -0700

    local changes

--- clothing-stock-management/src/components/Login.jsx
+++ clothing-stock-management/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 
-function Login({setAuthenticated}) {
+function Login() {
     const [employeeId, setEmployeeId] = useState("");
     const [pin, setPin] = useState("");
     const navigate = useNavigate();
