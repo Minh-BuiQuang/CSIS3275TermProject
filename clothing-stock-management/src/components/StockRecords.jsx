d21ab96498c94f305c61d002cef3f10a25c093da
Minh Bui <bqminh1806@gmail.com>
2022-03-18 21:30:53 -0700

    Reload data when switching to stock records

--- clothing-stock-management/src/components/StockRecords.jsx
+++ clothing-stock-management/src/components/StockRecords.jsx
@@ -1,42 +1,48 @@
-import {useContext} from 'react';
-import InventoryContext from '../Context/InventoryContext';
+import {useEffect, useState} from 'react';
 
 function StockRecords() {
-    const {transactions} = useContext(InventoryContext);
+    
+    const [transactions, setTransactions] = useState([]);
 
-    if(transactions == null)     
-        return ""
-    else
-        return (
-            <table className="table table-striped data-table">
-                <thead className="data-table">
-                    <tr className="data-table">
-                        <th scope='col' width='5%'>ID</th>
-                        <th scope='col' width='19%'>Product</th>
-                        <th scope='col' width='10%'>Employee</th>
-                        <th scope='col' width='5%'>Type</th>
-                        <th scope='col' width='15%'>Entity</th>
-                        <th scope='col' width='10%'>Time</th>
-                        <th scope='col' width='11%'>Date</th>
-                        <th scope='col' width='7%'>Quantity</th>
-                        <th scope='col'>Comments</th>
-                    </tr>
-                </thead>
-                <tbody className="data-table">
-                    {transactions.sort((a,b) => b.transactionNumber - a.transactionNumber).map(d=>(<tr key={d.transactionNumber}>
-                        <td width='5%'>{d.transactionNumber}</td>
-                        <td width='20%'>{d.productName}</td>
-                        <td width='10%'>{d.employeeName}</td>
-                        <td width='5%'>{d.type}</td>
-                        <td width='15%'>{d.supplierName == null ? (d.customerName == null ? "-" : d.customerName) : d.supplierName}</td>
-                        <td width='10%'>{new Date(d.date).toLocaleTimeString()}</td>
-                        <td width='12%'>{new Date(d.date).toLocaleDateString()}</td>
-                        <td width='7%' style={{textAlign: 'right', paddingRight:'40px'}}>{d.quantity}</td>
-                        <td>{d.comments}</td>
-                    </tr>))}
-                </tbody>
-            </table>
-        )
+    useEffect(()=>{
+        const fetchTransactions = async () => {
+            const response = await fetch('https://localhost:44348/api/Stock');
+            const data = await response.json();
+            setTransactions(data.data);
+        }
+        fetchTransactions();
+    }, [])
+
+    return (
+        <table className="table table-striped data-table">
+            <thead className="data-table">
+                <tr className="data-table">
+                    <th scope='col' width='5%'>ID</th>
+                    <th scope='col' width='19%'>Product</th>
+                    <th scope='col' width='10%'>Employee</th>
+                    <th scope='col' width='5%'>Type</th>
+                    <th scope='col' width='15%'>Entity</th>
+                    <th scope='col' width='10%'>Time</th>
+                    <th scope='col' width='11%'>Date</th>
+                    <th scope='col' width='7%'>Quantity</th>
+                    <th scope='col'>Comments</th>
+                </tr>
+            </thead>
+            <tbody className="data-table">
+                {transactions.sort((a,b) => b.transactionNumber - a.transactionNumber).map(d=>(<tr key={d.transactionNumber}>
+                    <td width='5%'>{d.transactionNumber}</td>
+                    <td width='20%'>{d.productName}</td>
+                    <td width='10%'>{d.employeeName}</td>
+                    <td width='5%'>{d.type}</td>
+                    <td width='15%'>{d.supplierName == null ? (d.customerName == null ? "-" : d.customerName) : d.supplierName}</td>
+                    <td width='10%'>{new Date(d.date).toLocaleTimeString()}</td>
+                    <td width='12%'>{new Date(d.date).toLocaleDateString()}</td>
+                    <td width='7%' style={{textAlign: 'right', paddingRight:'40px'}}>{d.quantity}</td>
+                    <td>{d.comments}</td>
+                </tr>))}
+            </tbody>
+        </table>
+    )
 }
 
 export default StockRecords;
