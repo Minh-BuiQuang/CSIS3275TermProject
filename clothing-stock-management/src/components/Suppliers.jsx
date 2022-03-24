0d89a5b87454ab7b4e192c6de04f4556f626d966
Jasmine Kaur <kaurj153@student.douglascollege.ca>
2022-03-23 17:41:07 -0700

    Supplier UI

--- clothing-stock-management/src/components/Suppliers.jsx
+++ clothing-stock-management/src/components/Suppliers.jsx
@@ -27,25 +27,27 @@ function Suppliers() {
     return (
         <>
             <div>
-                <div className='d-flex justify-content-between'>
-                <h3>Suppliers</h3>
+            <div className='d-flex justify-content-between'>
+            <div className="container d-flex mt-2 mb-2">
+            <h4 className="flex-grow-1" >Suppliers</h4>
                     <input type="text" className="form-control w-25" placeholder="Search" value={search} onChange={handleSearch}  />
                 </div>
-                <table className="table table-striped data-table">
-                    <thead className="data-table">
-                        <tr className="data-table">
-                            <th scope='col' width='10%'>ID</th>
-                            <th scope='col'>Supplier Name</th>
-                            <th scope='col'>Email</th>
-                            <th scope='col'>Phone</th>
+                </div>
+                <table className="table table-striped data-table container table-responsive d-flex flex-column mt-3">
+                <thead className="data-table">
+                    <tr className="row m-0">
+                            <th scope='col' className='col-2'>Supplier ID</th>
+                            <th scope='col' className='col-4'>Supplier Name</th>
+                            <th scope='col' className='col-4 px-0'>Email</th>
+                            <th scope='col' className='col-2 px-0'>Phone</th>
                         </tr>
                     </thead>
-                    <tbody className="data-table">
-                        {data.map(d=>(<tr key={d.supplierId}>
-                                <td width='10%'>{d.supplierId}</td>
-                                <td>{d.supplierName}</td>
-                                <td>{d.email}</td>
-                                <td>{d.phone}</td>
+                    <tbody className="scroll w-auto">
+                        {data.map(d=>(<tr key={d.supplierId} className="row m-0">
+                                <td scope='col' className='col-2'>{d.supplierId}</td>
+                                <td scope='col' className='col-4'>{d.supplierName}</td>
+                                <td scope='col' className='col-4'>{d.email}</td>
+                                <td scope='col' className='col-2'>{d.phone}</td>
                         </tr>))}
                     </tbody>
                 </table>
